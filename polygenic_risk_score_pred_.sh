0cd5478948ac2e6afbf32693205e781655b233b5
Max Lam <mlam@broadinstitute.org>
2020-08-26 00:47:09 -0400

    Changes committed

--- polygenic_risk_score_pred_.sh
+++ polygenic_risk_score_pred_.sh
@@ -237,7 +237,7 @@
                             pcafile=$VALUE
                             ;;
                     --coco)
-                            coco==$VALUE 
+                            coco=$VALUE 
                             ;;
                     --prs_predictors)
                             prs_predictors=$VALUE
